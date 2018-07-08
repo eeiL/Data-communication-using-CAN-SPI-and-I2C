adae2eab86c4330b366a0777a48ead7f66e8b1de
eeiL <38582045+eeiL@users.noreply.github.com>
2018-07-08 17:41:35 +0200

    Update I2C-Master.c

--- I2C-Master.c
+++ I2C-Master.c
@@ -6,7 +6,7 @@
 
 #define dataLength  8
 
-FlexCAN CAN_Umsteigen(500000);
+FlexCAN CAN_Transfer(500000);
 static CAN_message_t txmsg,rxmsg;
 
 int led = 13;
@@ -20,7 +20,7 @@ int returnData0[dataLength], returnData1[dataLength],returnData2[dataLength];
 
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
-  CAN_Umsteigen.begin();
+  CAN_Transfer.begin();
   pinMode(led,OUTPUT);
   Serial.begin(115200);
   void Write(int ch);
@@ -29,13 +29,15 @@ void setup() {
   Timer1.attachInterrupt(ECU);
   Timer3.initialize(10000);
   Timer3.attachInterrupt(I2C);
-  Serial.println("Master ready");
+  Serial.println("Master is ready");
 }
 
 
 void ECU(void){
-  while(CAN_Umsteigen.read(rxmsg)){   
-      if(rxmsg.id == 0x175){
+  while(CAN_Transfer.read(rxmsg)){   
+      
+      //CAN Bus date with ID=0x100
+      if(rxmsg.id == 0x100){
         for(int i=0; i<rxmsg.len; i++) {
         Data0[i] = rxmsg.buf[i];}
 
@@ -46,65 +48,67 @@ void ECU(void){
         Wire.endTransmission();    // stop transmitting   
       }
       
-      if(rxmsg.id == 0x179){
+      if(rxmsg.id == 0x101){
         for(int i=0; i<rxmsg.len; i++) {
         Data1[i] = rxmsg.buf[i];}
 
-        Wire.beginTransmission(10); // transmit to device #8
+        Wire.beginTransmission(10); // transmit to device #10
         for(int i=0; i<dataLength; i++){
         Wire.send(Data1[i]);
         }
         Wire.endTransmission();    // stop transmitting    
       }
       
-      if(rxmsg.id == 0x17D){
+      if(rxmsg.id == 0x102){
         for(int i=0; i<rxmsg.len; i++) {
         Data2[i] = rxmsg.buf[i];}
         
-        Wire.beginTransmission(12); // transmit to device #8
+        Wire.beginTransmission(12); // transmit to device #12
         for(int i=0; i<dataLength; i++){
         Wire.send(Data2[i]);
         }
         Wire.endTransmission();    // stop transmitting
       }
   }
-  //flag_ecu = 1;
+  flag_ecu = 1;
   digitalWrite(led, LOW);
 }
 
 void I2C(void){
   txmsg.len = 8;
   
+  //date from slave1 with CAN Date ID=0x285
   Wire.requestFrom(8,8);
   while(Wire.available()) {
     txmsg.id = 0x285;
     for(int i=0; i<dataLength; i++){
       returnData0[i]= Wire.receive();
       txmsg.buf[i]= returnData0[i];}
-    CAN_Umsteigen.write(txmsg);
+    CAN_Transfer.write(txmsg);
   }
 
+  //date from slave2 with CAN Date ID=0x289
   Wire.requestFrom(10,8);
   while(Wire.available()) {
     txmsg.id = 0x289;
     for(int i=0; i<dataLength; i++){
       returnData1[i]= Wire.receive();
       txmsg.buf[i]= returnData1[i];}
-    CAN_Umsteigen.write(txmsg);
+    CAN_Transfer.write(txmsg);
   }
 
+  //date from slave3 with CAN Date ID=0x280
   Wire.requestFrom(12,8);
   while(Wire.available()) {
     txmsg.id = 0x28D;
     for(int i=0; i<dataLength; i++){
       returnData2[i]= Wire.receive();
       txmsg.buf[i]= returnData2[i];}
-    CAN_Umsteigen.write(txmsg);
+    CAN_Transfer.write(txmsg);
   }
  
   flag_i2c = 1;
   digitalWrite(led, HIGH);
-  //digitalWrite(led, !digitalRead(led));
 }
 
 void loop() {
@@ -112,11 +116,10 @@ void loop() {
     Serial.println("Command from ECU:");
     Serial.print("");
   }
+  
   if(flag_i2c){
     Serial.println("Data from SPI Slave:");
     for(int i=0; i<dataLength; i++){
-      //Serial.print(returnData0[i],HEX);
-      //Serial.print(", ");
       Serial.print(returnData1[i],HEX);
       Serial.print(", ");
       Serial.println(returnData2[i],HEX);
@@ -128,9 +131,9 @@ void loop() {
 
 
 void Write(int ch){
-  while(CAN_Umsteigen.read(rxmsg)){
+  while(CAN_Transfer.read(rxmsg)){
     if(ch == 8){
-      if(rxmsg.id == 0x175){
+      if(rxmsg.id == 0x100){
         for(int i=0; i<rxmsg.len; i++) {
         Data[i] = rxmsg.buf[i];}
       }
@@ -142,11 +145,11 @@ void Write(int ch){
     }
     
     if(ch == 10){
-      if(rxmsg.id == 0x179){
+      if(rxmsg.id == 0x101){
         for(int i=0; i<rxmsg.len; i++) {
         Data[i] = rxmsg.buf[i];}
       }
-      Wire.beginTransmission(ch); // transmit to device #8
+      Wire.beginTransmission(ch); // transmit to device #10
       for(int i=0; i<dataLength; i++){
       Wire.send(Data[i]);
       }
@@ -154,11 +157,11 @@ void Write(int ch){
     }
     
     if(ch == 12){
-      if(rxmsg.id == 0x17D){
+      if(rxmsg.id == 0x102){
         for(int i=0; i<rxmsg.len; i++) {
         Data[i] = rxmsg.buf[i];}
       }
-      Wire.beginTransmission(ch); // transmit to device #8
+      Wire.beginTransmission(ch); // transmit to device #12
       for(int i=0; i<dataLength; i++){
       Wire.send(Data[i]);
       }
